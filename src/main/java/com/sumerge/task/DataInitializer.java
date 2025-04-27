c79a7f5e96d1e0c9cf7ca9f98f8585295ad3a4b5
Daniel Mina <dmina@sumerge.com>
2025-04-28 02:29:52 +0300

    update DataInitializer to add more data to db

--- src/main/java/com/sumerge/task/DataInitializer.java
+++ src/main/java/com/sumerge/task/DataInitializer.java
@@ -1,10 +1,14 @@
 package com.sumerge.task;
 
-import com.sumerge.task.models.Author;
+import com.sumerge.task.models.Assessment;
 import com.sumerge.task.models.Course;
+import com.sumerge.task.models.Rating;
+import com.sumerge.task.models.Author;
 import com.sumerge.task.models.Lang;
+import com.sumerge.task.repositories.AssessmentRepository;
 import com.sumerge.task.repositories.AuthorRepository;
 import com.sumerge.task.repositories.CourseRepository;
+import com.sumerge.task.repositories.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,15 +22,19 @@ import java.util.List;
 public class DataInitializer implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
-
     private final CourseRepository courseRepository;
-
+    private final AssessmentRepository assessmentRepository;
+    private final RatingRepository ratingRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DataInitializer(AuthorRepository authorRepository, CourseRepository courseRepository, PasswordEncoder passwordEncoder) {
+    public DataInitializer(AuthorRepository authorRepository, CourseRepository courseRepository,
+                           AssessmentRepository assessmentRepository, RatingRepository ratingRepository,
+                           PasswordEncoder passwordEncoder) {
         this.authorRepository = authorRepository;
         this.courseRepository = courseRepository;
+        this.assessmentRepository = assessmentRepository;
+        this.ratingRepository = ratingRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -53,6 +61,49 @@ public class DataInitializer implements CommandLineRunner {
         c5.setAuthors(List.of(clara, daniel));
 
         courseRepository.saveAll(List.of(c1, c2, c3, c4, c5));
-    }
 
+        Assessment assessment1 = new Assessment();
+        assessment1.setContent("Midterm Assessment for Java Basics");
+        assessment1.setCourse(c1);
+
+        Assessment assessment2 = new Assessment();
+        assessment2.setContent("Final Assessment for Spring Boot Advanced");
+        assessment2.setCourse(c2);
+
+        Assessment assessment3 = new Assessment();
+        assessment3.setContent("Final Assessment for JavaScript Essentials");
+        assessment3.setCourse(c3);
+
+        Assessment assessment4 = new Assessment();
+        assessment4.setContent("Midterm Assessment for Fullstack App with React and Spring");
+        assessment4.setCourse(c4);
+
+        Assessment assessment5 = new Assessment();
+        assessment5.setContent("Final Assessment for Node.js Backend Fundamentals");
+        assessment5.setCourse(c5);
+
+        assessmentRepository.saveAll(List.of(assessment1, assessment2, assessment3, assessment4, assessment5));
+
+        Rating rating1 = new Rating();
+        rating1.setNumber(5);
+        rating1.setCourse(c1);
+
+        Rating rating2 = new Rating();
+        rating2.setNumber(4);
+        rating2.setCourse(c2);
+
+        Rating rating3 = new Rating();
+        rating3.setNumber(3);
+        rating3.setCourse(c3);
+
+        Rating rating4 = new Rating();
+        rating4.setNumber(4);
+        rating4.setCourse(c4);
+
+        Rating rating5 = new Rating();
+        rating5.setNumber(5);
+        rating5.setCourse(c5);
+
+        ratingRepository.saveAll(List.of(rating1, rating2, rating3, rating4, rating5));
+    }
 }
